610672d95e7cfc856e66ec75702ab1cbad0377b0
dipanker <dipanker_kr@outlook.com>
2023-04-18 23:40:25 +0530

    added profile to test

--- src/test/java/com/dipankr/todobe/TodoBEApplicationTests.java
+++ src/test/java/com/dipankr/todobe/TodoBEApplicationTests.java
@@ -1,15 +1,12 @@
 package com.dipankr.todobe;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("prod")
 class TodoBEApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
