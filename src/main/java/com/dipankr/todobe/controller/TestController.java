db20bb2273a1a8d8053a7f9e0c99d25af42eb692
dipanker <dipanker_kr@outlook.com>
2023-04-30 13:22:34 +0530

    updated documentation for the project

--- src/main/java/com/dipankr/todobe/controller/TestController.java
+++ src/main/java/com/dipankr/todobe/controller/TestController.java
@@ -12,12 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    /**
+     * Endpoint to test the status of the server
+     *
+     * @return a message indicating that the server is up
+     */
     @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity<?> testRootEndpoint() {
         return getResponseJson(null, null, "Todo API is up and running.", HttpStatus.OK);
     }
 
-
+    /**
+     * Endpoint to test the status of the server <br/> Indicates that paths other than root are accessible
+     *
+     * @return a message indicating that server is up and different paths are accessible
+     */
     @GetMapping(value = "/test", produces = "application/json")
     public ResponseEntity<?> testApiEndpoint() {
         return getResponseJson(null, null, "/test is responsive..", HttpStatus.OK);
